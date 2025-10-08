0f510848eb76aac2fd8166cc117abc893172b098
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-10-08 19:40:15 +0700

    implement isPalindrome function logic

--- src/main/java/palindrome_number/Solution.java
+++ src/main/java/palindrome_number/Solution.java
@@ -2,8 +2,18 @@ package palindrome_number;
 
 public class Solution {
     public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
 
-        return false;
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x /= 10;
+        }
+
+        return x == reversedHalf || x == reversedHalf / 10;
     }
 
 }
