afbf8bfe34898e01914b417d15faeac35b73f527
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-10-18 09:44:51 +0700

    more simple solution for finding square root of an number

--- src/main/java/sqrt_x/Solution.java
+++ src/main/java/sqrt_x/Solution.java
@@ -2,23 +2,10 @@ package sqrt_x;
 
 public class Solution {
     public int mySqrt(int x) {
-        if (x < 2) return x;
-        int left = 0;
-        int right = x / 2;
-        int result = 0;
-
-        while (left <= right) {
-            int guess = (left + right) / 2;
-            long square = (long) guess * guess;
-            if (square == x) return guess;
-            if (square > x) {
-                right = guess - 1;
-            }
-            if (square < x) {
-                result = guess;
-                left = guess + 1;
-            }
+        long i = 1;
+        while ((i * i) <= x) {
+            i++;
         }
-        return result;
+        return (int) i - 1;
     }
 }
