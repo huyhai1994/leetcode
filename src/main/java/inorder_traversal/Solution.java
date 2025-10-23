1c06669fa50e8ba77847973a9de5203e66d87ab7
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-10-23 20:54:35 +0700

    implement inorderTraversal method for single node handling and add corresponding tests

--- src/main/java/inorder_traversal/Solution.java
+++ src/main/java/inorder_traversal/Solution.java
@@ -1,5 +1,11 @@
 package inorder_traversal;
 
-public class Solution {
+import java.util.List;
 
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        boolean isHasOneNode = root.left == null && root.right == null && root.val != 0;
+        if (isHasOneNode) return List.of(root.val);
+        return List.of();
+    }
 }
--- src/test/java/inorder_traversal/SolutionTest.java
+++ src/test/java/inorder_traversal/SolutionTest.java
@@ -1,6 +1,25 @@
 package inorder_traversal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 class SolutionTest {
+    @Test
+    void oneNode() {
+        TreeNode root = new TreeNode(1);
+        List<Integer> expected = List.of(1);
+        Solution solution = new Solution();
+        assertIterableEquals(expected, solution.inorderTraversal(root));
+    }
+
+    @Test
+    void emptyNode() {
+        TreeNode root = new TreeNode();
+        List<Integer> expected = List.of();
+        Solution solution = new Solution();
+        assertIterableEquals(expected, solution.inorderTraversal(root));
+    }
 }
