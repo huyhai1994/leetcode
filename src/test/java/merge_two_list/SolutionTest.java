989c743a14100f7e7666a988bbcc87fbbc3e322c
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-10-27 06:40:30 +0700

    update test cases for merging two lists: rename existing test and add new test for list with value of one

--- src/test/java/merge_two_list/SolutionTest.java
+++ src/test/java/merge_two_list/SolutionTest.java
@@ -38,9 +38,21 @@ class SolutionTest {
     }
 
     @Test
-    void oneEmptyListWithZeroValue() {
+    void oneEmptyListAndOneListHasZeroValue() {
         list2.val = 0;
         expectedList.val = 0;
+        mergeList = new Solution().mergeTwoLists(list1, list2);
+        while (expectedList != null) {
+            assertEquals(expectedList.val, mergeList.val);
+            mergeList = mergeList.next;
+            expectedList = expectedList.next;
+        }
+    }
+
+    @Test
+    void oneEmptyListAndOneListHasValueIsOne() {
+        list2.val = 1;
+        expectedList.val = 1;
         mergeList = new Solution().mergeTwoLists(list1, list2);
         while (expectedList != null) {
             assertEquals(expectedList.val, mergeList.val);
