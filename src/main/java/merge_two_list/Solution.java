a813e940527d280d88fdca0ff4f4320ba1946d41
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-10-27 06:42:36 +0700

    refactor mergeTwoLists method to handle cases where lists contain non-zero values

--- src/main/java/merge_two_list/Solution.java
+++ src/main/java/merge_two_list/Solution.java
@@ -1,7 +1,13 @@
 package merge_two_list;
 
 public class Solution {
-    public ListNode mergeTwoLists(ListNode List1, ListNode List2) {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 != null && list1.val != 0) {
+            return list1;
+        }
+        if (list2 != null && list2.val != 0) {
+            return list2;
+        }
         return new ListNode();
     }
 }
