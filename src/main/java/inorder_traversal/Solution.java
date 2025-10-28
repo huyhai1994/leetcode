46da6d77227cc40beb5bfcf1e140d69598bb8281
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-10-29 06:07:40 +0700

    refactor code for inorderTraversal

--- src/main/java/inorder_traversal/Solution.java
+++ src/main/java/inorder_traversal/Solution.java
@@ -5,22 +5,16 @@ import java.util.List;
 
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        if (root == null || root.val == 0) return List.of();
-        boolean isHasOneNode = root.val != 0 && root.left == null && root.right == null;
         List<Integer> result = new ArrayList<>();
-        if (isHasOneNode) return List.of(root.val);
         inOrder(root, result);
         return result;
     }
 
     public void inOrder(TreeNode node, List<Integer> result) {
         if (node == null) return;
-
         inOrder(node.left, result);
         result.add(node.val);
         inOrder(node.right, result);
-
-
     }
 }
 
