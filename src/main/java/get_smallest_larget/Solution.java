457b267314514335747845857539cc7231c60471
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-11-08 15:11:47 +0700

    guard for input value

--- src/main/java/get_smallest_larget/Solution.java
+++ src/main/java/get_smallest_larget/Solution.java
@@ -2,6 +2,8 @@ package get_smallest_larget;
 
 public class Solution {
     public static String getSmallestAndLargest(String s, int k) {
+        if (s == null) throw new RuntimeException("String must be not null");
+        if (s.isEmpty() || k <= 0 || s.length() <= k) throw new RuntimeException("bad request");
         String largest = "";
         String smallest = "";
 
--- src/test/java/get_smallest_larget/SolutionTest.java
+++ src/test/java/get_smallest_larget/SolutionTest.java
@@ -1,7 +1,23 @@
 package get_smallest_larget;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SolutionTest {
+
+    @Test
+    void nullString() {
+        int k = 2;
+        assertThrows(RuntimeException.class, () -> Solution.getSmallestAndLargest(null, k));
+    }
+
+    @Test
+    void twoCharString() {
+        String s = "ab";
+        int k = 2;
+        assertThrows(RuntimeException.class, () -> Solution.getSmallestAndLargest(s, k));
+    }
+
 
 }
