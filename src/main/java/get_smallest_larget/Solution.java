161cc9e7600a0cbf76889f876664e0111460b28b
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-11-08 14:58:26 +0700

    smallest and largest problem hacker rank

---
+++ src/main/java/get_smallest_larget/Solution.java
@@ -0,0 +1,11 @@
+package get_smallest_larget;
+
+public class Solution {
+    public static String getSmallestAndLargest(String s, int k) {
+        String largest = "";
+        String smallest = "";
+
+        return smallest + "/n" + largest;
+
+    }
+}
---
+++ src/test/java/get_smallest_larget/SolutionTest.java
@@ -0,0 +1,7 @@
+package get_smallest_larget;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+}
