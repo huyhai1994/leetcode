b0b3e9d2f6d18d4ddc5d86a518fb5d5880580494
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-11-10 22:03:11 +0700

    [Hacker Rank]: start solving anagram problem.

---
+++ src/main/java/anagram/Solution.java
@@ -0,0 +1,7 @@
+package anagram;
+
+public class Solution {
+    static boolean isAnagram(String a, String b) {
+        return false;
+    }
+}
---
+++ src/test/java/anagram/SolutionTest.java
@@ -0,0 +1,7 @@
+package anagram;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+}
