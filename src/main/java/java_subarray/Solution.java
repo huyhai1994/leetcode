3067a88e3821ad578c461d03e89182723402682c
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-11-19 06:32:21 +0700

    refactor using enhanced for loop

--- src/main/java/java_subarray/Solution.java
+++ src/main/java/java_subarray/Solution.java
@@ -3,8 +3,8 @@ package java_subarray;
 public class Solution {
     public static int printNegativeSumOfSubarray(int[] array) {
         int count = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 0) {
+        for (int j : array) {
+            if (j < 0) {
                 count++;
             }
         }
