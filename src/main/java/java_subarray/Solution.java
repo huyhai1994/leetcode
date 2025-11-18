95abec6a5b6e65705490dac5490c3f43018c7346
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-11-19 06:27:07 +0700

    test case for 1 item

--- src/main/java/java_subarray/Solution.java
+++ src/main/java/java_subarray/Solution.java
@@ -1,4 +1,13 @@
 package java_subarray;
 
 public class Solution {
+    public static int printNegativeSumOfSubarray(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/java_subarray/SolutionTest.java
+++ src/test/java/java_subarray/SolutionTest.java
@@ -1,7 +1,19 @@
 package java_subarray;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
+    @Test
+    void caseOneItemWithOneNegativeCountArray() {
+        int[] array = new int[]{-1};
+        assertEquals(1, Solution.printNegativeSumOfSubarray(array));
+    }
 
+    @Test
+    void caseOneItemWitNonNegativeCountArray() {
+        int[] array = new int[]{1};
+        assertEquals(0, Solution.printNegativeSumOfSubarray(array));
+    }
 }
