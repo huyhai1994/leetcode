50dda7625a01a9bc78a13f439193befd45938545
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-11-21 06:28:29 +0700

    print output to STDOUT

--- src/main/java/java_array_list/Solution.java
+++ src/main/java/java_array_list/Solution.java
@@ -7,21 +7,30 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int d;
-        int q;
+        int d, q, queryTime, x, y;
         ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             d = sc.nextInt();
-            StringBuilder printOutPut = new StringBuilder(d + " ");
+            StringBuilder printout = new StringBuilder(d + " ");
             ArrayList<Integer> array = new ArrayList<>();
             for (int j = 0; j < d; j++) {
                 q = sc.nextInt();
                 array.add(q);
-                printOutPut.append(q).append(" ");
+                printout.append(q).append(" ");
             }
-            System.out.println(printOutPut);
+            System.out.println(printout);
             arrayList.add(array);
         }
-        System.out.println(arrayList);
+
+        queryTime = sc.nextInt();
+        for (int i = 0; i < queryTime; i++) {
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+            try {
+                System.out.println(arrayList.get(x).get(y));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("ERROR");
+            }
+        }
     }
 }
