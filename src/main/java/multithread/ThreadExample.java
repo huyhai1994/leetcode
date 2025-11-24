64eec56d8b2e4af596be85b41601d2774fedab2c
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-11-24 20:24:16 +0700

    implementing Runnable interface

--- src/main/java/multithread/ThreadExample.java
+++ src/main/java/multithread/ThreadExample.java
@@ -5,6 +5,9 @@ public class ThreadExample {
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
         myThread.start();
+        MyRunnable runnable = new MyRunnable();
+        Thread newThread = new Thread(runnable);
+        newThread.start();
     }
 }
 
@@ -12,10 +15,22 @@ class MyThread extends Thread {
     @Override
     public void run() {
         int i = 0;
-        while (i < 100000) {
-            System.out.println("I'm running ...." + i++);
+        while (i < 1000) {
+            System.out.println(Thread.currentThread() + "I'm running ...." + i++);
         }
         System.out.println("I'm shutting down, Goodbye ");
     }
 
+}
+
+class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (i < 1000) {
+            System.out.println(Thread.currentThread() + "I'm running ...." + i++);
+        }
+        System.out.println("I'm shutting down, Goodbye ");
+    }
 }
