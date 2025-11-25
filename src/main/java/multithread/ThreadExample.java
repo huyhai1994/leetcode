880fc7e390ef9d4a70a4caf41aac942064610010
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-11-25 21:04:33 +0700

    Html Crawling example.

--- src/main/java/multithread/ThreadExample.java
+++ src/main/java/multithread/ThreadExample.java
@@ -9,7 +9,7 @@ public class ThreadExample {
         myThread.start();
         Thread newThread = new Thread(() -> {
             int i = 0;
-            while (i < 1000) {
+            while (i < 10) {
                 System.out.println(Thread.currentThread() + "I'm running ...." + i++);
                 try {
                     TimeUnit.SECONDS.sleep(1);
@@ -20,14 +20,25 @@ public class ThreadExample {
             System.out.println("I'm shutting down, Goodbye ");
         });
         newThread.start();
+        try {
+            myThread.join(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
 class MyThread extends Thread {
+
     @Override
     public void run() {
         int i = 0;
-        while (i < 1000) {
+        while (i < 10) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             System.out.println(Thread.currentThread() + "I'm running ...." + i++);
         }
         System.out.println("I'm shutting down, Goodbye ");
@@ -35,14 +46,3 @@ class MyThread extends Thread {
 
 }
 
-class MyRunnable implements Runnable {
-
-    @Override
-    public void run() {
-        int i = 0;
-        while (i < 1000) {
-            System.out.println(Thread.currentThread() + "I'm running ...." + i++);
-        }
-        System.out.println("I'm shutting down, Goodbye ");
-    }
-}
