b14c933b1541ccf6c9d38ca32ecf573dfe5cd300
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-11-26 16:58:59 +0700

    change to AtomicInteger for more thread safe.

--- src/main/java/multithread/Counter.java
+++ src/main/java/multithread/Counter.java
@@ -1,10 +1,12 @@
 package multithread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
-    private int counter = 0;
+    private final AtomicInteger counter = new AtomicInteger(0);
 
 
     public int getNextCounter() {
-        return counter++;
+        return counter.getAndIncrement();
     }
 }
