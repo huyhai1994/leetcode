c47f4b9289d794adc7eb5910c6ff0392ee2f2616
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-12-08 20:35:39 +0700

    using stream api

--- src/main/java/comparator/Player.java
+++ src/main/java/comparator/Player.java
@@ -2,6 +2,8 @@ package comparator;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Checker implements Comparator<Player> {
     @Override
@@ -24,19 +26,38 @@ public class Player {
     }
 
     public static void main(String[] args) {
-        Player[] players = new Player[5];
-        players[0] = new Player("amy", 100);
-        players[1] = new Player("david", 150);
-        players[2] = new Player("heraldo", 50);
-        players[3] = new Player("aakansha", 75);
-        players[4] = new Player("amy", 100);
+//        Player[] players = new Player[5];
+//        players[0] = new Player("amy", 100);
+//        players[1] = new Player("david", 150);
+//        players[2] = new Player("heraldo", 50);
+//        players[3] = new Player("aakansha", 75);
+//        players[4] = new Player("amy", 100);
+//
+//        Checker checker = new Checker();
+//        Arrays.sort(players, checker);
+//        for (Player player : players) {
+//            System.out.println(player.name + " " + player.score);
+//        }
 
-        Checker checker = new Checker();
-        Arrays.sort(players, checker);
-        for (Player player : players) {
-            System.out.println(player.name + " " + player.score);
-        }
+        List<Player> players = Arrays.asList(
 
+                new Player("amy", 100),
+                new Player("david", 150),
+                new Player("heraldo", 50),
+                new Player("aakansha", 75),
+                new Player("amy", 100)
+        );
+        List<String> names = players.stream().sorted((a, b) -> {
+                    if (a.score != b.score) {
+                        return b.score - a.score;
+                    }
+                    return a.name.compareToIgnoreCase(b.name);
+                })
+                .map(player -> player.name)
+                .collect(Collectors.toList());
+        names.forEach(
+                System.out::println
+        );
     }
 
 
