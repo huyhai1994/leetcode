141e5f0104b83dda7caa8e159a4f2c8e210afdf9
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-12-11 21:25:59 +0700

    finish get from console

--- src/main/java/visitor_pattern/Solution.java
+++ src/main/java/visitor_pattern/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
@@ -12,16 +13,27 @@ public class Solution {
         int numberOfNode;
         List<Integer> nodeValues = new ArrayList<>();
         List<Integer> nodeColors = new ArrayList<>();
+        List<int[]> edges = new ArrayList<>();
         try {
             numberOfNode = Integer.parseInt(reader.readLine());
-            for (int i = 0; i < numberOfNode; i++) {
-                nodeValues.add(Integer.parseInt(reader.readLine()));
+            String lineValues = reader.readLine();
+            List<String> valueParts = Arrays.asList(lineValues.split(" "));
+            valueParts.forEach(value -> nodeValues.add(Integer.parseInt(value)));
+
+            String lineColors = reader.readLine();
+            List<String> colorParts = Arrays.asList(lineColors.split(" "));
+            colorParts.forEach(color -> nodeColors.add(Integer.parseInt(color)));
+
+            for (int i = 0; i < numberOfNode - 1; i++) {
+                String edgeLine = reader.readLine();
+                String[] edgeParts = edgeLine.split(" ");
+                int u = Integer.parseInt(edgeParts[0]);
+                int v = Integer.parseInt(edgeParts[1]);
+                edges.add(new int[]{u, v});
             }
-            for (int i = 0; i < numberOfNode; i++) {
-                nodeColors.add(Integer.parseInt(reader.readLine()));
-            }
-            nodeValues.forEach(System.out::println);
-            nodeColors.forEach(System.out::println);
+            System.out.println(nodeValues);
+            System.out.println(nodeColors);
+            System.out.println(edges);
 
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
