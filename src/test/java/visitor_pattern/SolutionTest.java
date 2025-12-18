1e7d2e7b801b103d4fe3c183530d399a48b02163
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-12-19 06:54:06 +0700

    refactor the test result

--- src/test/java/visitor_pattern/SolutionTest.java
+++ src/test/java/visitor_pattern/SolutionTest.java
@@ -3,6 +3,8 @@ package visitor_pattern;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,7 +17,7 @@ class SolutionTest {
     void oneNodeTree() {
         List<Integer> values = Collections.singletonList(1);
         List<Color> colors = Collections.singletonList(Color.RED);
-        List<Integer>[] paths = new List[]{Collections.emptyList()};
+        List<Integer>[] paths = new ArrayList[1];
         Solution sol = new Solution(values, colors, paths);
         Tree tree = new TreeLeaf(1, Color.RED, 0);
         Tree result = sol.buildTree(1);
@@ -30,17 +32,20 @@ class SolutionTest {
     @Test
     @DisplayName("two node tree, one root and one leaf")
     void twoNodeTree() {
-//        Solution sol = new Solution();
-//        List<Integer> values = Arrays.asList(1, 2);
-//        List<Color> colors = Arrays.asList(Color.RED, Color.GREEN);
-//        List<Integer>[] paths = new List[]{Arrays.asList(1, 2)};
-//        List<Tree> buildTree = sol.buildTree(values, colors, paths);
-//        List<Tree> treeExpected = Arrays.asList(
-//                new TreeNode(1, Color.RED, 0),
-//                new TreeLeaf(2, Color.GREEN, 1));
-//        assertEquals(treeExpected.size(), buildTree.size());
-//        for (int i = 0; i < treeExpected.size(); i++) {
-//            assertEquals(treeExpected.get(i), buildTree.get(i));
-//        }
+        List<Integer> values = Arrays.asList(1, 2);
+        List<Color> colors = Arrays.asList(Color.RED, Color.GREEN);
+        List<Integer>[] paths = new ArrayList[3]; // index 0 unused
+        paths[1] = new ArrayList<>();
+        paths[2] = new ArrayList<>();
+
+        paths[1].add(2);
+        paths[2].add(1);
+        Solution sol = new Solution(values, colors, paths);
+        Tree buildTree = sol.buildTree(1);
+        TreeNode root = new TreeNode(1, Color.RED, 0);
+        TreeLeaf leaf = new TreeLeaf(2, Color.GREEN, 1);
+        root.addChildren(leaf);
+        System.out.println(buildTree);
+        System.out.println(root);
     }
 }
