a6aeab0523164f9d63d0d13521e3ae8f3fdeac08
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-12-21 00:47:13 +0700

    add more test cases

--- src/test/java/number_to_binary/SolutionTest.java
+++ src/test/java/number_to_binary/SolutionTest.java
@@ -1,7 +1,5 @@
 package number_to_binary;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -47,5 +45,73 @@ class SolutionTest {
         int number = 10;
         String expectedResult = "1010";
         assertEquals(expectedResult, Solution.numberToBinary(number));
+    }
+
+    // Additional test cases added for better coverage
+    @Test
+    void inputAs0Case() {
+        int number = 0;
+        String expectedResult = "0";
+        assertEquals(expectedResult, Solution.numberToBinary(number));
+    }
+
+    @Test
+    void inputAs5Case() {
+        int number = 5;
+        String expectedResult = "101";
+        assertEquals(expectedResult, Solution.numberToBinary(number));
+    }
+
+    @Test
+    void inputAs6Case() {
+        int number = 6;
+        String expectedResult = "110";
+        assertEquals(expectedResult, Solution.numberToBinary(number));
+    }
+
+    @Test
+    void inputAs7Case() {
+        int number = 7;
+        String expectedResult = "111";
+        assertEquals(expectedResult, Solution.numberToBinary(number));
+    }
+
+    @Test
+    void inputAs8Case() {
+        int number = 8;
+        String expectedResult = "1000";
+        assertEquals(expectedResult, Solution.numberToBinary(number));
+    }
+
+    @Test
+    void inputAs16Case() {
+        int number = 16;
+        String expectedResult = "10000";
+        assertEquals(expectedResult, Solution.numberToBinary(number));
+    }
+
+    @Test
+    void inputAs31Case() {
+        int number = 31;
+        String expectedResult = "11111";
+        assertEquals(expectedResult, Solution.numberToBinary(number));
+    }
+
+    @Test
+    void inputAs1023Case() {
+        int number = 1023; // 2^10 - 1 -> ten 1's
+        assertEquals(Integer.toBinaryString(number), Solution.numberToBinary(number));
+    }
+
+    @Test
+    void inputAs1024Case() {
+        int number = 1024; // 2^10 -> 1 followed by ten 0's
+        assertEquals(Integer.toBinaryString(number), Solution.numberToBinary(number));
+    }
+
+    @Test
+    void inputAsMaxIntCase() {
+        int number = Integer.MAX_VALUE;
+        assertEquals(Integer.toBinaryString(number), Solution.numberToBinary(number));
     }
 }
