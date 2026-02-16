e61d386a8146c2f6113d5c9b19e1a27a25525b2d
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-02-16 18:02:47 +0700

    draw diagram for countdown latch

--- src/main/java/multithread/countdownlatch/Main.java
+++ src/main/java/multithread/countdownlatch/Main.java
@@ -7,30 +7,31 @@ public class Main {
     public static void main(String[] args) {
         CountDownLatch latch = new CountDownLatch(2);
         new Thread(
-                () -> {
-                    System.out.println("Task 1 done!");
-                    latch.countDown();
-                }
+                Main.task(latch)
         ).start();
         new Thread(
-                () -> {
-                    try {
-                        TimeUnit.SECONDS.sleep(20);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.println("Task 2 done!");
-                    latch.countDown();
-                }
+                Main.task(latch)
         ).start();
         boolean isFinished;
         try {
-            isFinished = latch.await(10, TimeUnit.SECONDS);
+            isFinished = latch.await(1, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         if (isFinished) System.out.println("All task finished!!!");
         else System.out.println("Not all task finished!!!");
 
+    }
+
+    public static Runnable task(CountDownLatch latch) {
+        return () -> {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Task  done!");
+            latch.countDown();
+        };
     }
 }
