9644cc232e8139d0a9a2495d82c8d9538e8df3b6
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-02-17 18:53:51 +0700

    Example to demonstrate Byte InputStream and OutputStream in Java

---
+++ src/main/java/input_output/InputOutputStream.java
@@ -0,0 +1,30 @@
+package input_output;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class InputOutputStream {
+    public static void main(String[] args) {
+        byte[] bWrite = {11, 21, 3, 40, 5};
+        try (OutputStream os = Files.newOutputStream(Paths.get("test.txt"))) {
+            for (byte b : bWrite) {
+                os.write(b);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (InputStream is = Files.newInputStream(Paths.get("test.txt"))) {
+            int size = is.available();
+            for (int i = 0; i < size; i++) {
+                System.out.println((char) is.read() + "");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
