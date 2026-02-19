a0eb3acb7e06cbb1e2cdfe24a19636e835c9e99c
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-02-19 20:55:37 +0700

    Experimenting with temporary files

---
+++ src/main/java/nio/TempFileDemo.java
@@ -0,0 +1,14 @@
+package nio;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TempFileDemo {
+    public static void main(String[] args) throws IOException {
+        System.getProperty("java.io.tmpdir");
+        File temp = File.createTempFile("text", ".txt");
+        System.out.println(temp);
+        temp.deleteOnExit();
+
+    }
+}
