3515dde41660c387010cd2222ff7c61acd82157d
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-02-19 19:28:39 +0700

    print root info

---
+++ src/main/java/nio/DumpRoots.java
@@ -0,0 +1,13 @@
+package nio;
+
+import java.io.File;
+
+public class DumpRoots {
+    public static void main(String[] args) {
+        File[] roots = File.listRoots();
+        for (File root : roots) {
+            System.out.println(root);
+        }
+    }
+}
+
