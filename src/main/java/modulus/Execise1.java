08accea4ec556ddcc3dfc948c945fe20c4e11efb
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-04-18 19:05:12 +0700

    [feat]: modulus exercise#1.

---
+++ src/main/java/modulus/Execise1.java
@@ -0,0 +1,13 @@
+package modulus;
+
+public class Execise1 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 15; i++) {
+            String output = i + " % " + 5 + " = " + i % 5;
+            System.out.println(output);
+
+        }
+    }
+
+}
