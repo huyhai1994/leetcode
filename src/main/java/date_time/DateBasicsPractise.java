39eef726267901dee0d7d85d7ef5960e5ac314c2
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-05-05 21:04:54 +0700

    [feat]: add basic structure

---
+++ src/main/java/date_time/DateBasicsPractise.java
@@ -0,0 +1,11 @@
+package date_time;
+
+import java.time.LocalDate;
+
+public class DateBasicsPractise {
+
+    public LocalDate getToday() {
+        return null;
+    }
+
+}
---
+++ src/test/java/date_time/DateBasicsPractiseTest.java
@@ -0,0 +1,16 @@
+package date_time;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateBasicsPractiseTest {
+    private final DateBasicsPractise service = new DateBasicsPractise();
+
+    @Test
+    void should_return_today() {
+
+
+    }
+
+}
