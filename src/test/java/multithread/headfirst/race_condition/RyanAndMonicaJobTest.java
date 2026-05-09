d9ee8184a5474d657f0c7ef1511d0873fde3cd07
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-05-09 14:57:59 +0700

    [feat]: add test

--- src/test/java/multithread/headfirst/race_condition/RyanAndMonicaJobTest.java
+++ src/test/java/multithread/headfirst/race_condition/RyanAndMonicaJobTest.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RyanAndMonicaJobTest {
     @Test
-    void ryan_and_monica_should_fight_for_same_resource() {
+    void ryan_and_monica_should_fight_for_same_resource() throws InterruptedException {
         BankAccount account = new BankAccount();
         RyanAndMonicaJob ryan = new RyanAndMonicaJob("Ryan", account, 50);
         RyanAndMonicaJob monica = new RyanAndMonicaJob("Monica", account, 100);
@@ -17,6 +18,8 @@ class RyanAndMonicaJobTest {
         executorService.execute(ryan);
         executorService.execute(monica);
         executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+        assertEquals(0, account.getBalance());
     }
 
 }
