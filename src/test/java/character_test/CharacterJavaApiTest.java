2b7127cb6b7751531b8e5ced681e089510eee0be
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-07-06 14:25:27 +0700

    [test]: Java Character API

---
+++ src/test/java/character_test/CharacterJavaApiTest.java
@@ -0,0 +1,67 @@
+package character_test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CharacterJavaApiTest {
+
+    @Test
+    void whenCharacterIsLetter_thenReturnTrue() {
+        assertTrue(Character.isLetter('c'));
+    }
+
+    @Test
+    void whenCharacterIsNotLetter_thenReturnFalse() {
+        assertFalse(Character.isLetter('1'));
+    }
+
+    @Test
+    void whenCharacterIsDigit_thenReturnTrue() {
+        assertTrue(Character.isDigit('1'));
+    }
+
+    @Test
+    void whenCharacterIsNotDigit_thenReturnFalse() {
+        assertFalse(Character.isDigit('c'));
+    }
+
+    @Test
+    void whenCharacterIsLetterOrDigit_ThenReturnTrue() {
+        assertAll(
+                () -> assertTrue(Character.isLetterOrDigit('C')),
+                () -> assertTrue(Character.isLetterOrDigit('1'))
+        );
+    }
+
+    @Test
+    void whenIsWhiteSpace_thenReturnTrue() {
+        assertTrue(Character.isWhitespace(' '));
+        assertTrue(Character.isWhitespace('\n'));
+        assertTrue(Character.isWhitespace('\t'));
+    }
+
+    @Test
+    void whenSubtractCharacterZero_thenReturnDigit() {
+        assertEquals(8, '8' - '0');
+        assertEquals(9, '9' - '0');
+    }
+
+    @Test
+    void whenConvertCharacterToDigit_thenReturnCharacter() {
+        assertEquals('8', (char) (8 + '0'));
+    }
+
+    @Test
+    void whenConvertLowerCaseToUpperCase_thenReturnUpperCase() {
+        assertEquals('C', Character.toUpperCase('c'));
+    }
+
+    @Test
+    void whenConcertUpperCaseToLowerCase_thenReturnLowerCase() {
+        assertEquals('c', Character.toLowerCase('C'));
+    }
+}
