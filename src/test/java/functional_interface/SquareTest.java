9f649d098dd997826d29d1993df1b16e80ef65f0
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-08-31 20:55:01 +0700

    [Functional Interface] : predicate example

--- src/test/java/functional_interface/SquareTest.java
+++ src/test/java/functional_interface/SquareTest.java
@@ -2,6 +2,12 @@ package functional_interface;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class SquareTest {
@@ -10,6 +16,15 @@ class SquareTest {
         int a = 5;
         Square square = (x) -> x * x;
         assertThat(square.calculate(a)).isEqualTo(25);
+    }
+
+    @Test
+    void predicate() {
+        List<String> strings = Arrays.asList("A", "B", "C", "A");
+        Predicate<String> predicate = (s) -> s.startsWith("A");
+        List<String> result = strings.stream().filter(predicate).collect(Collectors.toList());
+        assertThat(result).isEqualTo(Arrays.asList("A", "A"));
+
     }
 
 }
